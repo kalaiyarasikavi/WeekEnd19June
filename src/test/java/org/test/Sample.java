4f44ae438be44efe98624b9225be778b72dca185
kalaiyarasi <kalaiyarasikgc@gmail.com>
2022-06-20 12:57:45 +0530

    kavi update code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -3,6 +3,11 @@ package org.test;
 public class Sample {
 	public static void main(String[] args) {
 		
+		System.out.println("Test1");
+		System.out.println("Test4");
+		System.out.println("Test5");
+		
+		
 	}
 
 }
